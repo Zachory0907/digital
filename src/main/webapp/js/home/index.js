7f178920977e8ea8c99ed39a23e238bacaa95512
Zachory0907 <15712998762@163.com>
2016-04-15 14:50:41 +0800

    # 我擦

--- src/main/webapp/js/home/index.js
+++ src/main/webapp/js/home/index.js
@@ -1,3 +1,10 @@
 angular.module('app', []).controller('indexController', function($scope, $http){
 	$scope.msg = "欢迎";
+
+	/*鼠标放到新闻上显示效果*/
+	$("#news ul li").mouseover(function(){
+		$(this).addClass("newBarHover");
+	}).mouseout(function(){
+		$(this).removeClass("newBarHover");
+	});
 });
