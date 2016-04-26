38a8c637ec8a92ff73a10b8eb290eeeea0208709
Zachory0907 <15712998762@163.com>
2016-04-26 08:42:16 +0800

    + 首页增加news功能

--- src/main/webapp/js/home/index.js
+++ src/main/webapp/js/home/index.js
@@ -7,4 +7,20 @@ angular.module('app', []).controller('indexController', function($scope, $http){
 	}).mouseout(function(){
 		$(this).removeClass("newBarHover");
 	});
+
+	/*news随鼠标动而变化*/
+	$("#news ul li").hover(function(){
+		var index = $(this).index();
+		$(".Content").eq(index).show().siblings(".Content").hide();
+		$(this).find("a").addClass("ys").parent().siblings().find("a").removeClass("ys");
+		$("#news ul li").eq(index).addClass("newBarHover").siblings().removeClass("newBarHover");
+	});
+
+	/*news选中背景变化*/
+	$(".Content dl").mouseover(function(){
+		$(this).addClass("hover");
+	}).mouseout(function(){
+		$(this).removeClass("hover");
+	});
+
 });
